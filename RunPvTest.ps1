e1918398aa004c02501a96af0218506b77e4c1fa
Playtool <106289250+Playtool@users.noreply.github.com>
2025-05-20 14:39:19 +0530

    Delete RunPvTest.ps1

--- RunPvTest.ps1
+++
@@ -1,3 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-iex (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/Playtool/test/main/test.ps1' -Headers @{ Authorization = "Bearer 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" })
