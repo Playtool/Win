98e0fc96b0738e6bf1b73a2044626d9d348f2164
Playtool <106289250+Playtool@users.noreply.github.com>
2025-05-20 11:00:57 +0000

    Apply Explorer customizations and optimizations for Windows 11 Pro

--- Models/NotificationOptimizations.ps1
+++ Models/NotificationOptimizations.ps1
@@ -1,281 +1,31 @@
-GetNotificationOptimizations()
-{
-return new OptimizationGroup
-{
-Name = "Notifications",
-Category = OptimizationCategory.Notifications,
-Settings = new List
-{
-new OptimizationSetting
-{
-Id = "notifications-toast",
-Name = "Windows Notifications",
-Description = "Controls toast notifications",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications",
-Name = "ToastEnabled",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls toast notifications",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-},
-new OptimizationSetting
-{
-Id = "notifications-sound",
-Name = "Notification Sounds",
-Description = "Controls notification sounds",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings",
-Name = "NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls notification sounds",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-},
-new OptimizationSetting
-{
-Id = "notifications-toast-above-lock",
-Name = "Notifications On Lock Screen",
-Description = "Controls notifications above lock screen",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings",
-Name = "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls notifications on lock screen",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-},
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications",
-Name = "LockScreenToastEnabled",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls notifications on lock screen",
-IsPrimary = false,
-AbsenceMeansEnabled = true
-}
-},
-LinkedSettingsLogic = LinkedSettingsLogic.All
-},
-new OptimizationSetting
-{
-Id = "notifications-critical-toast-above-lock",
-Name = "Show Reminders and VoIP Calls Notifications",
-Description = "Controls critical notifications above lock screen",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings",
-Name = "NOC_GLOBAL_SETTING_ALLOW_CRITICAL_TOASTS_ABOVE_LOCK",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls critical notifications above lock screen",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-},
-new OptimizationSetting
-{
-Id = "notifications-security-maintenance",
-Name = "Security and Maintenance Notifications",
-Description = "Controls security and maintenance notifications",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance",
-Name = "Enabled",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls security and maintenance notifications",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-},
-new OptimizationSetting
-{
-Id = "notifications-capability-access",
-Name = "Capability Access Notifications",
-Description = "Controls capability access notifications",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.CapabilityAccess",
-Name = "Enabled",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls capability access notifications",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-},
-new OptimizationSetting
-{
-Id = "notifications-startup-app",
-Name = "Startup App Notifications",
-Description = "Controls startup app notifications",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.StartupApp",
-Name = "Enabled",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls startup app notifications",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-},
-new OptimizationSetting
-{
-Id = "notifications-system-setting-engagement",
-Name = "System Setting Engagement Notifications",
-Description = "Controls system setting engagement notifications",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "SOFTWARE\Microsoft\Windows\CurrentVersion\UserProfileEngagement",
-Name = "ScoobeSystemSettingEnabled",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls system setting engagement notifications",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-},
-new OptimizationSetting
-{
-Id = "notifications-clock-change",
-Name = "Clock Change Notifications",
-Description = "Controls clock change notifications",
-Category = OptimizationCategory.Notifications,
-GroupName = "System Notifications",
-IsEnabled = false,
-ControlType = ControlType.BinaryToggle,
-RegistrySettings = new List
-{
-new RegistrySetting
-{
-Category = "Notifications",
-Hive = RegistryHive.CurrentUser,
-SubKey = "Control Panel\Desktop",
-Name = "DstNotification",
-RecommendedValue = 0,
-EnabledValue = 1,
-DisabledValue = 0,
-ValueType = RegistryValueKind.DWord,
-DefaultValue = 1,
-Description = "Controls clock change notifications",
-IsPrimary = true,
-AbsenceMeansEnabled = true
-}
-}
-}
-}
-};}
-}
-}
-}
+# --- Notification Optimizations for Windows 11 Pro ---
+
+# 1. Disable toast (banner) notifications
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Value 0 -Type DWord
+
+# 2. Disable notification sounds
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND" -Value 0 -Type DWord
+
+# 3. Disable notifications above lock screen
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK" -Value 0 -Type DWord
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "LockScreenToastEnabled" -Value 0 -Type DWord
+
+# 4. Disable critical notifications above lock screen (reminders, VoIP calls)
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_ALLOW_CRITICAL_TOASTS_ABOVE_LOCK" -Value 0 -Type DWord
+
+# 5. Disable Security and Maintenance notifications
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance" -Name "Enabled" -Value 0 -Type DWord
+
+# 6. Disable Capability Access notifications
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.CapabilityAccess" -Name "Enabled" -Value 0 -Type DWord
+
+# 7. Disable Startup App notifications
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.StartupApp" -Name "Enabled" -Value 0 -Type DWord
+
+# 8. Disable System Setting Engagement notifications
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\UserProfileEngagement" -Name "ScoobeSystemSettingEnabled" -Value 0 -Type DWord
+
+# 9. Disable Clock Change notifications (DST changes)
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "DstNotification" -Value 0 -Type DWord
+
+# --- End of Notification Optimizations ---
